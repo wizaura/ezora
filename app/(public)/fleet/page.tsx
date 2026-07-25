4a844519bfe4aac808fb10dd70b6f627ee3ae8c2
Melbin Mathew <melbinppmathewp@gmail.com>
2026-07-25 21:26:36 +0530

    3rd commit - Added rental service page 1.2.9

--- app/(public)/fleet/page.tsx
+++ app/(public)/fleet/page.tsx
@@ -20,9 +20,15 @@ export default async function FleetPage() {
         // }
     );
 
+    console.log(res.status);
+    console.log(res.url);
+
+    const text = await res.text();
+    console.log(text);
+
     const { data: activeCategories } = await res.json();
 
-    console.log(activeCategories,'ss')
+    console.log(activeCategories, 'ss')
 
     return (
         <>
